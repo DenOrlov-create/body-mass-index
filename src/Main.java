7a5dfebb684a7bfe1a97177aabb34eead921059a
Denis Orlov <spartak20002000@gmail.com>
2024-09-19 14:07:44 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+
+        double weight = 98;
+        double height = 1.87;
+
+        int bmiIndex = service.calculate(weight, height);
+
+        System.out.println("BMI: " + bmiIndex);
+    }
+}
